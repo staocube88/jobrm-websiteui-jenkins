be6df87f60b713716790b344e50410c50873492d
Garun Vagidov <garun@garunski.com>
2018-09-06 22:32:45 -0700

    cleanup and testing node for running e2e

--- protractor.conf.js
+++ protractor.conf.js
@@ -1,25 +1,10 @@
-// Protractor configuration file, see link for more information
-// https://github.com/angular/protractor/blob/master/lib/config.ts
-
-const { SpecReporter } = require('jasmine-spec-reporter');
-const { JUnitXmlReporter } = require('jasmine-reporters');
-
 exports.config = {
   allScriptsTimeout: 11000,
   specs: ['./e2e/**/*.e2e-spec.ts'],
   capabilities: {
-    browserName: 'chrome',
-    chromeOptions: {
-      args: [
-        '--headless',
-        '--disable-gpu',
-        //'--window-size=800x600',
-        '--no-sandbox'
-      ]
-    }
+    browserName: 'chrome'
   },
-  directConnect: false,
-  seleniumAddress: 'http://localhost:4444/wd/hub',
+  directConnect: true,
   baseUrl: 'http://localhost:4200/',
   framework: 'jasmine',
   jasmineNodeOpts: {
@@ -31,15 +16,8 @@ exports.config = {
     require('ts-node').register({
       project: 'e2e/tsconfig.e2e.json'
     });
-
     jasmine
       .getEnv()
       .addReporter(new SpecReporter({ spec: { displayStacktrace: true } }));
-
-    var junitReporter = new JUnitXmlReporter({
-      savePath: './e2e/test-results/',
-      consolidateAll: false
-    });
-    jasmine.getEnv().addReporter(junitReporter);
   }
 };
